a5cf35f242d2aacb3d520490c9e94d87336d4a39
nuuls <nilsv21@gmail.com>
2022-12-02 05:49:20 +0100

    im scared

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -69,4 +72,164 @@ func filterLines(input string) []Line {
 
 	// Return the filtered lines
 	return filteredLines
+}
+
+const apiEndpoint = "https://chat.openai.com/backend-api/conversation"
+
+// Payload represents the payload to be sent to the API endpoint
+type Payload struct {
+	Action          string    `json:"action"`
+	Messages        []Message `json:"messages"`
+	ParentMessageID string    `json:"parent_message_id"`
+	Model           string    `json:"model"`
+}
+
+// Message represents a message in the payload
+type Message struct {
+	ID      string      `json:"id"`
+	Role    string      `json:"role"`
+	Content interface{} `json:"content"`
+}
+
+// Content represents the content of a message in the payload
+type Content struct {
+	ContentType string   `json:"content_type"`
+	Parts       []string `json:"parts"`
+}
+
+func callAPI() {
+	// Define the payload
+	payload := Payload{
+		Action:          "next",
+		Messages:        []Message{},
+		ParentMessageID: "",
+		Model:           "",
+	}
+
+	// Add the message to the payload
+	// payload.Messages = append(payload.Messages, createMessage())
+
+	// Set the parent message ID and model in the payload
+	payload.ParentMessageID = "a4a85e2c-61ee-4907-9a83-ed1155e784de"
+	payload.Model = "text-davinci-002-render"
+
+	// Marshal the payload into JSON
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		// Handle error
+		return
+	}
+
+	// Create a new POST request
+	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		// Handle error
+		return
+	}
+
+	// Set the request headers
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// Handle error
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// Handle error
+		return
+	}
+
+	// Parse the response
+	var response map[string]interface{}
+	json.Unmarshal(body, &response)
+
+	// Process the response
+	// (You can replace this with your own code to process the response)
+	fmt.Println("Response:", response)
+}
+
+// Response represents the response from the API
+type Response struct {
+	Message        ResMessage `json:"message"`
+	ConversationID string     `json:"conversation_id"`
+	Error          error      `json:"error"`
+}
+
+// Message represents a message in the response
+type ResMessage struct {
+	ID         string                 `json:"id"`
+	Role       string                 `json:"role"`
+	User       interface{}            `json:"user"`
+	CreateTime interface{}            `json:"create_time"`
+	UpdateTime interface{}            `json:"update_time"`
+	Content    ResContent             `json:"content"`
+	EndTurn    interface{}            `json:"end_turn"`
+	Weight     float64                `json:"weight"`
+	Metadata   map[string]interface{} `json:"metadata"`
+	Recipient  string                 `json:"recipient"`
+}
+
+// Content represents the content of a message in the response
+type ResContent struct {
+	ContentType string        `json:"content_type"`
+	Parts       []interface{} `json:"parts"`
+}
+
+// Define a function for decoding an EventSource http.Response
+func decodeEventSourceResponse(resp *http.Response) (*Response, error) {
+	// Check the response status code to make sure it's 200 (OK)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected HTTP status code: %d", resp.StatusCode)
+	}
+
+	// Check the Content-Type header to make sure it's an EventSource response
+	contentType := resp.Header.Get("Content-Type")
+	if contentType != "text/event-stream" {
+		return nil, fmt.Errorf("unexpected Content-Type: %s", contentType)
+	}
+
+	// Read the response body as a byte slice
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Split the response body into separate lines
+	lines := bytes.Split(responseBody, []byte("\n"))
+
+	// Loop through the lines and decode the last non-empty line
+	var response *Response
+	for i := len(lines) - 1; i >= 0; i-- {
+		// Skip empty lines
+		if len(lines[i]) == 0 {
+			continue
+		}
+
+		// Check if the line is "data: [DONE]"
+		if bytes.Equal(lines[i], []byte("data: [DONE]")) {
+			// Skip the "data: [DONE]" line
+			continue
+		}
+
+		// Strip the "data: " prefix from the line
+		line := bytes.TrimPrefix(lines[i], []byte("data: "))
+
+		// Decode the line into a Response object
+		response = &Response{}
+		if err := json.Unmarshal(line, response); err != nil {
+			return nil, err
+		}
+
+		// Stop the loop when a non-empty line is successfully decoded
+		break
+	}
+
+	return response, nil
 }
